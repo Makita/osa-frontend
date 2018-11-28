1c08ec2561fee9194f8ec886774f14578ff3006f
Makita <justin.choi.000@gmail.com>
2018-11-28 14:31:54 -0700

    Use Redux dev tool and namespace the reducer

--- src/index.js
+++ src/index.js
@@ -2,21 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
 import appointments from "Reducers/appointments"; // eslint-disable-line import/no-unresolved
 
 import App from "./app";
 
-// eslint-disable-next-line import/prefer-default-export
-export const ReduxContext = React.createContext(createStore(appointments));
+const mainReducer = combineReducers({ appointments });
 
 ReactDOM.render(
   (
-    <ReduxContext.Provider>
+    <Provider store={createStore(
+      mainReducer,
+      // eslint-disable-next-line no-underscore-dangle
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}>
       <Router>
           <App />
       </Router>
-    </ReduxContext.Provider>
+    </Provider>
   ),
   document.getElementById("root")
 );
